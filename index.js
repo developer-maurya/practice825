bed1f81f6210b209ce3fe25e1ee80c7234d605a1
Chitranjan kumar <chitranjan75075@gmail.com>
2025-08-25 22:00:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-// function chit (){
-//     console.log("hello");
+ function chit (){
+    console.log("hello");
     
-// }
-// setTimeout(chit,4000)
+ }
+setTimeout(chit,4000)
+
+ console.log("one");
+ console.log("two");
+ setTimeout(()=>{
+     console.log("hello")
+ },4000)
+ console.log("three");
+ console.log("four")
 
 
-// console.log("one");
-// console.log("two");
-// setTimeout(()=>{
-//     console.log("hello")
-// },4000)
-// console.log("three");
-// console.log("four")
 
+ function sum (a,b){
+     console.log(a+b);
+ }
+ function calculator (a,b, sumcallback){
+  sumcallback(a,b)
+ }
 
+ calculator (1,2 ,(a,b)=>{
+     console.log(a+b);
+ })
+ calculator (1,2 ,sum)
 
-// function sum (a,b){
-//     console.log(a+b);
-// }
-// function calculator (a,b, sumcallback){
-//  sumcallback(a,b)
-// }
-
-// calculator (1,2 ,(a,b)=>{
-//     console.log(a+b);
-// })
-// calculator (1,2 ,sum)
-
-// let age =9;
-// if (age>=18){
-//     if(age>=60){
-//         console.log("senir")
-//     }else{
-//         console.log("middle");
-//     }
-// }else{
-// console.log("child")
-// }
+ let age =9;
+ if (age>=18){
+     if(age>=60){
+         console.log("senir")
+     }else{
+        console.log("middle");
+     }
+ }else{
+ console.log("child")
+ }
 
 
 const datas=[
@@ -60,3 +59,4 @@ datas.forEach(loop);
 function loop(value){
 document.write(value)
 }
+
